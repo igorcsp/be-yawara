02c34f415c1035d7b7c7387d57d9a1560226c950
igorcsp <icordeiro787@gmail.com>
2025-02-13 19:52:39 -0300

    .

--- routes/products.js
+++ routes/products.js
@@ -14,8 +14,6 @@ router.get('/', async (req, res) => {
 });
 
 router.post("/", [auth, admin], upload.array("images"), async (req, res) => {
-    console.log(req.body); // form data que o endpoint está recebendo
-
     const { error } = validateProduct(req.body);
     if (error) return res.status(400).send(error.details);
 
